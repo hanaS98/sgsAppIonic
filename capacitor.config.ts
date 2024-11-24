5a10e81c975be2b20d11b5edbb0a206102772ebf
hanaS98 <85634043+hanaS98@users.noreply.github.com>
2024-11-24 08:59:33 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SGSAPP',
+  webDir: 'www',
+};
+
+
+export default config;
