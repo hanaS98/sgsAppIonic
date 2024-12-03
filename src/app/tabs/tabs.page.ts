3df15c2e07f807443983b2a8df04899170f18e35
hanaS98 <hsaeed0014@stu.kau.edu.sa>
2024-12-03 09:56:36 +0300

    fullsplashscreen

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss','tabs.page.css']
 })
-export class TabsPage {
+export class TabsPage implements DoCheck{
 
   constructor() {}
+  ngDoCheck(){
+    if(document.documentElement.lang === 'en'){
+      this.title1 = this.title1EN;
+      this.title2 = this.title2EN;
+    }else{
+      this.title1 = this.title1AR;
+      this.title2 = this.title2AR;
+    }
+  }
 
+  title1 = "الرئيسية";
+  title2 = "الخدمات";
+
+  title1AR = "الرئيسية";
+  title2AR = "الخدمات";
+
+  title1EN = "Home";
+  title2EN = "Services";
 }
